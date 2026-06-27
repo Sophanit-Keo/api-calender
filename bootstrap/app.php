81e334ab203e3f76f739f5dd87c1888b64b88359
Sophanit-Keo <sophanitkeo1803@gmail.com>
2026-06-27 23:29:55 +0700

    vercel implement

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,10 +5,14 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 
+$apiPrefix = getenv('APP_API_PREFIX');
+$apiPrefix = $apiPrefix === false ? 'api' : $apiPrefix;
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
+        apiPrefix: $apiPrefix,
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
@@ -17,6 +21,6 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
-            fn (Request $request) => $request->is('api/*'),
+            fn (Request $request) => $request->is('api/*') || $request->is('v1/*'),
         );
     })->create();
